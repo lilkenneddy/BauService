371c3ea3b2eae2ab6ef885aa4fe3ba102116f161
Rostyslav Skliaruk <sklyaruk.rostislav1903@gmail.com>
2025-06-13 12:30:33 +0200

    first commit

---
+++ src/main/java/codillas/BauService/BauServiceApplication.java
@@ -0,0 +1,13 @@
+package codillas.BauService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BauServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BauServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codillas/BauService/BauServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package codillas.BauService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BauServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
